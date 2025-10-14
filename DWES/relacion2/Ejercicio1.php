9fbd77e59593bbb761584bc9bb187a91258fa28c
albertogarciaquintana5 <albertogarciaquintana5@gmail.com>
2025-10-14 21:23:15 +0200

    Ejercicios 14/10/2025

--- DWES/relacion2/Ejercicio1.php
+++ DWES/relacion2/Ejercicio1.php
@@ -4,23 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio1</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container mt-5 p-4 border border-2 rounded-3" style="max-width: 400px;">
-        <h2 class="mb-4 text-center">Calculadora en PHP</h2>
+
+    <div>
+        <h2>Calculadora en PHP</h2>
         <form action="" method="get">
-            <div class="mb-3">
-                <label for="num1" class="form-label">Número 1:</label>
-                <input type="number" class="form-control" id="num1" name="num1" required>
+            <div>
+                <label for="num1" >Número 1:</label>
+                <input type="number" id="num1" name="num1" required>
             </div>
             <div class="mb-3">
-                <label for="num2" class="form-label">Número 2:</label>
-                <input type="number" class="form-control" id="num2" name="num2" required>
+                <label for="num2" >Número 2:</label>
+                <input type="number" id="num2" name="num2" required>
             </div>
             <div class="mb-3">
-                <label for="operador" class="form-label">Operador:</label>
-                <select class="form-select" id="operador" name="operador" required>
+                <label for="operador" >Operador:</label>
+                <select id="operador" name="operador" required>
                     <option value="+">+</option>
                     <option value="-">-</option>
                     <option value="*">*</option>
@@ -28,7 +26,7 @@
                     <option value="%">%</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary w-100">Calcular</button>
+            <button type="submit">Calcular</button>
         </form>
 
     
