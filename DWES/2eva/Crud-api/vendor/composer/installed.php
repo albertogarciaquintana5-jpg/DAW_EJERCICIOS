ba701244f2083c73bbd38d2e69984e843c344906
albertogarciaquintana5-jpg <albertogarciaquintana5@gmail.com>
2026-01-29 14:56:07 +0100

    API REST

---
+++ DWES/2eva/Crud-api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alumno/crud-api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '99046e8c1e3fd24be17d38b323812aced32890fa',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alumno/crud-api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '99046e8c1e3fd24be17d38b323812aced32890fa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.11.1',
+            'version' => '6.11.1.0',
+            'reference' => 'd1e91ecf8c598d073d0995afa8cd5c75c6e19e66',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
